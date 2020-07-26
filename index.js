47ac06a015f5198d9e5e9ded4ab89b8cc72aeba7
Finn Pauls <mbp@pauls.dev>
2020-07-27 01:54:29 +0200

    get help with revealing the meaning

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import ReactDOM from "react-dom"
 import styled from 'styled-components'
 import {sample} from 'lodash'
 import {monographs} from './lib/hiragana'
+import {useHotkeys} from 'react-hotkeys-hook'
 
 const Center = styled.div`
     display: flex;
@@ -10,11 +11,11 @@ const Center = styled.div`
     justify-content: center;
     flex-direction: column;
     height: 100%;
+    font-family: sans-serif;
 `
 
 const Prompt = styled.div`
     font-size: 5em;
-    font-family: sans-serif;
 `
 
 const TextInput = styled.input`
@@ -22,22 +23,69 @@ const TextInput = styled.input`
     padding: 8px 12px;
     border-radius: 6px;
     border: 1px solid #ccc;
-    font-family: sans-serif;
     background-color: white;
 `
+
+const Validation = styled.div`
+    height: 1em;
+`
+
+const RevealButton = styled.button`
+    margin: 10px;
+`
+
+const TextInputContainer = styled.div`
+    height: 60px;
+`
+
+const Reveal = styled.div`
+    font-size: 1.2em;
+`
+
 
 function App () {
     const [current, setCurrent] = useState(getNewCharacter())
     const [input, setInput] = useState('')
+    const [isWrong, setIsWrong] = useState(false)
+    const [isRevealing, setIsRevealing] = useState(false)
+    useHotkeys('enter', () => { 
+        console.log('test', isRevealing)
+        if (isRevealing) {
+            setIsRevealing(false)
+        }
+    }, [isRevealing])
 
-    if (input.toLowerCase().trim() === current[1].toLowerCase()) {
+    if (input.trim() === '?') {
         setInput('')
-        setCurrent(getNewCharacter())
+        setIsRevealing(true)
+    }
+
+    if (input.trim().length === current[1].length) {
+        if (input.toLowerCase().trim() === current[1].toLowerCase()) {
+            setInput('')
+            setCurrent(getNewCharacter())
+            setIsWrong(false)   
+        } else {
+            setInput('')
+            setIsWrong(input.trim())
+        }
+    }
+    
+    function toggleReveal() {
+        setIsRevealing(!isRevealing)
     }
 
     return <Center>
         <Prompt>{current[0]}</Prompt>
-        <TextInput type='text' value={input} onChange={e => setInput(e.target.value)} />
+        <TextInputContainer>
+        {
+            isRevealing ? <Reveal>{current[1]}</Reveal> : <TextInput autoFocus type='text' value={input} onChange={e => setInput(e.target.value)} />
+        }
+        </TextInputContainer>
+        <Validation>
+            {isWrong ? `${isWrong} ❌`: ' '}
+            <RevealButton onClick={toggleReveal}>{isRevealing ? 'solve' : 'reveal'}</RevealButton>
+        </Validation> 
     </Center>
 }
 
