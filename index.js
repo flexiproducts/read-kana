96b65a0595c0cddba070098219e6340a6c2b0805
Finn Pauls <mbp@pauls.dev>
2020-08-08 02:14:05 +0200

    Add eslint

--- index.js
+++ index.js
@@ -1,96 +1,112 @@
-import React, {useState, useEffect} from "react"
-import ReactDOM from "react-dom"
+import React, {useState, useEffect} from 'react'
+import ReactDOM from 'react-dom'
 import styled from 'styled-components'
 import {sample} from 'lodash'
 import {monographs} from './lib/hiragana'
 import {useHotkeys} from 'react-hotkeys-hook'
 
 const Center = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    height: 100%;
-    font-family: sans-serif;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100%;
+  font-family: sans-serif;
 `
 
 const Prompt = styled.div`
-    font-size: 5em;
+  font-size: 5em;
 `
 
 const TextInput = styled.input`
-    all: unset;
-    padding: 8px 12px;
-    border-radius: 6px;
-    border: 1px solid #ccc;
-    background-color: white;
+  all: unset;
+  padding: 8px 12px;
+  border-radius: 6px;
+  border: 1px solid #ccc;
+  background-color: white;
 `
 
-const Validation = styled.div`
-    height: 1em;
-`
+const Validation = styled.div``
 
 const RevealButton = styled.button`
-    margin: 10px;
+  margin: 10px;
 `
 
 const TextInputContainer = styled.div`
-    height: 60px;
+  height: 60px;
 `
 
 const Reveal = styled.div`
-    font-size: 1.2em;
+  font-size: 1.2em;
 `
 
+function App() {
+  const [current, setCurrent] = useState(getNewCharacter())
+  const [input, setInput] = useState('')
+  const [isWrong, setIsWrong] = useState(false)
+  const [isRevealing, setIsRevealing] = useState(false)
+  const [numberCorrect, setNumberCorrect] = useState(0)
 
-function App () {
-    const [current, setCurrent] = useState(getNewCharacter())
-    const [input, setInput] = useState('')
-    const [isWrong, setIsWrong] = useState(false)
-    const [isRevealing, setIsRevealing] = useState(false)
-    useHotkeys('enter', () => { 
-        console.log('test', isRevealing)
-        if (isRevealing) {
-            setIsRevealing(false)
-        }
-    }, [isRevealing])
+  useHotkeys(
+    'enter',
+    () => {
+      console.log('test', isRevealing)
+      if (isRevealing) {
+        setIsRevealing(false)
+      }
+    },
+    [isRevealing]
+  )
 
-    if (input.trim() === '?') {
-        setInput('')
-        setIsRevealing(true)
+  if (input.trim() === '?') {
+    setInput('')
+    setIsRevealing(true)
+  }
+
+  if (input.trim().length === current[1].length) {
+    if (input.toLowerCase().trim() === current[1].toLowerCase()) {
+      setNumberCorrect((number) => number + 1)
+      setInput('')
+      setCurrent(getNewCharacter())
+      setIsWrong(false)
+    } else {
+      setInput('')
+      setIsWrong(input.trim())
     }
+  }
 
-    if (input.trim().length === current[1].length) {
-        if (input.toLowerCase().trim() === current[1].toLowerCase()) {
-            setInput('')
-            setCurrent(getNewCharacter())
-            setIsWrong(false)   
-        } else {
-            setInput('')
-            setIsWrong(input.trim())
-        }
-    }
-    
-    function toggleReveal() {
-        setIsRevealing(!isRevealing)
-    }
+  function toggleReveal() {
+    setIsRevealing(!isRevealing)
+  }
 
-    return <Center>
-        <Prompt>{current[0]}</Prompt>
-        <TextInputContainer>
-        {
-            isRevealing ? <Reveal>{current[1]}</Reveal> : <TextInput autoFocus type='text' value={input} onChange={e => setInput(e.target.value)} />
-        }
-        </TextInputContainer>
-        <Validation>
-            {isWrong ? `${isWrong} ❌`: ' '}
-            <RevealButton onClick={toggleReveal}>{isRevealing ? 'solve' : 'reveal'}</RevealButton>
-        </Validation> 
+  return (
+    <Center>
+      <Prompt>{current[0]}</Prompt>
+      <TextInputContainer>
+        {isRevealing ? (
+          <Reveal>{current[1]}</Reveal>
+        ) : (
+          <TextInput
+            autoFocus
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        )}
+      </TextInputContainer>
+      <Validation>
+        {isWrong ? `${isWrong} ❌` : ' '}
+        <RevealButton onClick={toggleReveal}>
+          {isRevealing ? 'solve' : 'reveal'}
+        </RevealButton>
+      </Validation>
+      <div>✅ {numberCorrect}</div>
     </Center>
+  )
 }
 
 function getNewCharacter() {
-    return sample(Object.entries(monographs))
+  return sample(Object.entries(monographs))
 }
 
-ReactDOM.render(<App />, document.getElementById("app"))
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ readme.md
@@ -0,0 +1,1 @@
+https://en.wiktionary.org/wiki/Appendix:1000_Japanese_basic_words
